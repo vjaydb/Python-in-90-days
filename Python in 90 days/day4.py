a40600783bb724a95e0539e4057a83122edf49b0
vjaydb <vijayboricha125@gmail.com>
2026-04-02 00:20:17 +0530

    d

--- Python in 90 days/day4.py
+++ Python in 90 days/day4.py
@@ -67,18 +67,18 @@ while outstanding_loan_amount >12500:
 bmi_values = [14.5, 17.2, 22.1, 27.4, 32.8, 37.1, 41.5]
 for i, bmi in enumerate(bmi_values,1):
     if bmi <16:
-        print(f"{i}: Severely Underweight")
+        print(f"{i}:{bmi} : Severely Underweight")
     if bmi >=16 and bmi <= 18.4:
-        print(f"{i}: Underweight")
+        print(f"{i}:{bmi} : Underweight")
     if bmi >=18.5 and bmi <=24.9:
-        print (f"{i}: Normal")
+        print (f"{i}:{bmi} : Normal")
     if bmi >=25 and bmi <=29.9:
-        print (f"{i}: Overweight")
+        print (f"{i}:{bmi} : Overweight")
     if bmi >=30 and bmi <=34.9:
-        print (f"{i}: Obsese Class 1")
+        print (f"{i}:{bmi} : Obsese Class 1")
     if bmi >=35 and bmi <=39.9:
-        print (f"{i}: Obese Class 2")
+        print (f"{i}:{bmi} : Obese Class 2")
     if bmi >=40:
-        print (f"{i}: Severely Obese")
+        print (f"{i}:{bmi} : Severely Obese")
 
            
