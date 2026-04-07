14d4f2f54218e7d40557904bf6bc6f114af02bef
vjaydb <vijayboricha125@gmail.com>
2026-04-07 21:59:14 +0530

    070426 2200

--- day7.py
+++ day7.py
@@ -10,10 +10,14 @@ raw_tickers = [
     "BSE:IRFC ",
 ]
 for trade in raw_tickers:
-    print(trade.strip())
-    print(trade.upper())
-
-    print(trade.)
+    #print(trade.strip())
+    #print(trade.strip().upper())
+    a=(trade.strip().split(":"))
+    print(a[:1])
+    #b=(a[:1])
+    #print(b.strip().upper)
+    #for exchange, stock in a:
+       # print(exchange.upper().strip())
 
 # Excersise 2, Write a function def parse_trade(trade_string) that takes a string like "Ather,750,830,50" and returns a dictionary with name, buy_price, current_price, quantity. Handle the case where the string is malformed (wrong number of parts).
 trades = [
@@ -26,5 +30,13 @@ trades = [
     "Zomato,90,110,abc",
     "IRCTC,720,800,20"
 ]
-
+for trade in trades:
+    trade_v=trade.strip().split(",")
+    for trade_v1  trade_v:
+        trade_v11=trade_v1.isalpha()
+        if trade_v11 == Truine:
+            stock_name=trade_v11 
+            print(stock_name)
+         
+         
 #print(trades.strip().split("-"))
