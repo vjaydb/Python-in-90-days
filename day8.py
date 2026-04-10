1856378e3f61a70dd9a49ca685af72429c70424d
vjaydb <vijayboricha125@gmail.com>
2026-04-10 09:37:32 +0530

    f

--- day8.py
+++ day8.py
@@ -1,5 +1,5 @@
 #File handling
-with open("text.text","w") as f:
+with open("text.txt","w") as f:
     f.write("Ather Energy, 609, 810/n")
     f.write("Suzlon, 86, 45,6000/n")
     f.write("Ahmednagar Forging, 150, 20, 500/n")
@@ -7,4 +7,8 @@ with open("text.text","w") as f:
     f.write("AshokLeyland, 30, 500, 50000")
     f.write("Lodha Devlopers, 500, 1200, 435")
     
-print(text.text)
+with open("text.txt", "r") as f:
+    for line in f:
+        trade=line.strip()
+print(trade)
+    
