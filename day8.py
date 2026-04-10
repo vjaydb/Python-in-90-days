a8bfdd6713f858d6c7151e7d6f30ce390e98a616
vjaydb <vijayboricha125@gmail.com>
2026-04-10 07:07:37 +0530

    100426 0700

--- day8.py
+++ day8.py
@@ -1,1 +1,10 @@
-#
+#File handling
+with open("text.text","w") as f:
+    f.write("Ather Energy, 609, 810/n")
+    f.write("Suzlon, 86, 45,6000/n")
+    f.write("Ahmednagar Forging, 150, 20, 500/n")
+    f.write("RVNL, 400,300, 5000/n")
+    f.write("AshokLeyland, 30, 500, 50000")
+    f.write("Lodha Devlopers, 500, 1200, 435")
+    
+print(text.text)
